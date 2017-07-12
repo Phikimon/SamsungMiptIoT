1283141a82fb77c6846421497fdfcf665622e603
Nikita Pettik <pettik@phystech.edu>
2017-07-12 16:56:01 +0300

    Some refactoring

--- AC/AC.py
+++ AC/AC.py
@@ -7,26 +7,24 @@ import datetime
 
 def sendConfigToServer(idDevice, modules, conf):
     client = mqtt.Client()
-    url = "artik.vdi.mipt.ru"
-    client.connect(url, 1883, 60)
+    serverURL = "artik.vdi.mipt.ru"
+    client.connect(serverURL, 1883, 60)
 
     for module in modules:
-        cmd = "mod " + module["name"] + " 1"
-        device = "devices/lora/"+idDevice 
+        cmd = "mod " + module["name"] + " " + "1"
+        device = "devices/lora/" + idDevice 
         client.publish(device + "/config", cmd, 1)
-        print module["data"]["names"][0]
         actualConfigs = {}
-        for prop in module["data"]["names"]:
-            if prop == "luminocity":
-                actualConfigs["sensor_period"] = conf["lighting"]["sensor_period"]
-            if prop == "lamp":
-                print conf["lighting"]
-            if prop == "pump":
-                print conf["watering"]
-            if prop == "humidity":
-                actualConfigs["sensor_period"] = conf["lighting"]["sensor_period"]
+        for attribute in module["data"]["names"]:
+            if attribute == "luminocity":
+                actualConfigs["period"] = conf["lighting"]["sensor_period"]
+            #if attribute == "lamp":
+            #    print conf["lighting"]
+            #if attribute == "pump":
+            #    print conf["watering"]
+            if attribute == "humidity":
+                actualConfigs["period"] = conf["watering"]["sensor_period"]
 
-        print actualConfigs
         for config in actualConfigs:
             cmd = "set " + config +  " " + str(actualConfigs[config])
             client.publish(device, cmd, 1)
@@ -44,6 +42,7 @@ def log(message):
     with open("AC.log", "a") as fileLog:
         fileLog.write(str(now) + " : " + message + "\n")
 
+
 def run(idDevice = "0"):
     log("Recieved new id device " + str(idDevice))
     if idDevice == "0":
@@ -54,18 +53,16 @@ def run(idDevice = "0"):
         jsonDev = json.loads(devFile.read())
         for device in jsonDev["devices"]:
             if idDevice == device["ID"]:
-                modules.append(device["modules"])
+                modules = device["modules"]
 
     jsonConf = {}
     with open("config.json", "r") as configFile:
         jsonConf = json.loads(configFile.read())
 
+    sendConfigToServer(idDevice, modules, jsonConf)
 
-    #print modules[0]
-    #print jsonConf
-    sendConfigToServer(idDevice, modules[0], jsonConf)
 
-def on_connect(client, userdata, rc, unUsable = 0):
+def on_connect(client, userdata, rc, unusable = 0):
     log("Connected with result code " + str(rc))
     client.subscribe("CM_AC")
 
@@ -73,17 +70,23 @@ def on_message(client, userdata, msg):
     run(msg.payload)
 
 
-
-if __name__ == "__main__":
-    run("807B8590200001E4")
-else:
+def listenLocalServer():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
     client.connect("127.0.0.1", 1883, 60)
 
     client.loop_forever()
+
+def tests():
+    run("807B8590200001E4")
+
+
+
+if __name__ == "__main__":
+    tests()
+else:
+    listenLocalServer()
 
 
 
